9598da386282c36ee569e4b71c66e904f57d42e1
Peter <46445810+Profute@users.noreply.github.com>
2024-02-05 19:04:51 +0100

    Added: Additional remaps to fugitive, for faster git commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,9 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>ga", ":G add ")
+vim.keymap.set("n", "<leader>gc", ":G commit ")
+vim.keymap.set("n", "<leader>gf", ":G fetch <CR>")
+vim.keymap.set("n", "<leader>gph", ":G push <CR>")
+vim.keymap.set("n", "<leader>gpl", ":G pull <CR>")
+vim.keymap.set("n", "<leader>gu", ":G gui <CR>")
+vim.keymap.set("n", "<leader>gh", ":G help <CR>")
+
