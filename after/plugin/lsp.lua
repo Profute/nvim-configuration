f26190f5c45d96dc7a47238c5bc669f9c3930307
Peter <46445810+Profute@users.noreply.github.com>
2024-02-05 19:07:10 +0100

    Configured lsp.lua: Now displays installed Mason LSP's such as linters, autocompletion and suggestions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,18 @@
 local lsp = require('lsp-zero')
 --require('lspconfig').lua_ls.setup({})
-require('mason').setup()
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  --ensure_installed = {''},
+  handlers = {
+    lsp.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  },
+})
 
---lsp.ensure_installed({])
+--lsp.ensure_installed({})
 
 --insert keymaps
 local cmp = require('cmp')
